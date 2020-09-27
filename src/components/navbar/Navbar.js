6b4156fdbbc58c55b8f279a8e48d774a2a91557b
Abul Kalam Azad <you@example.com>
2020-09-27 08:40:17 +0600

    CLI warnings resolved

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
